dbdb8b1c1d2c9ba26e47885ed254e34ea5554d91
Roman <Alimov.ramon@gmail.com>
2023-01-31 14:34:09 +0300

    lesson 5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, useLocation, useParams} from 'react-router-dom'
 import s from './Sidebar.module.css'
 import {PATH} from '../Pages'
 import closeIcon from './closeOutline.svg'
@@ -10,49 +10,59 @@ type PropsType = {
 }
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
+    const location = useLocation()
+    console.log(location.pathname)
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
     return (
         <>
             {/*затемнение справа от открытого меню*/}
-            {open && <div className={s.background} onClick={handleClose}/>}
+            {open && <div className={s.background} onClick={handleClose} />}
 
-            <aside className={sidebarClass}>
-                <button className={s.close} onClick={handleClose}>
-                    <img
-                        src={closeIcon}
-                        alt="close sidebar"
-                        id={'hw5-menu-close'}
-                    />
-                </button>
 
-                <nav id={'hw5-menu'} className={s.nav}>
-                    <NavLink
-                        id={'hw5-pre-junior-link'}
-                        to={PATH.PRE_JUNIOR}
-                        onClick={handleClose}
-                        // className={...} // делает студент
-                    >
-                        Pre-junior
-                    </NavLink>
-                    <NavLink
-                        id={'hw5-junior-link'}
-                        to={PATH.JUNIOR}
-                        onClick={handleClose}
-                        // className={...} // делает студент
-                    >
-                        Junior
-                    </NavLink>
-                    <NavLink
-                        id={'hw5-junior-plus-link'}
-                        to={PATH.JUNIOR_PLUS}
-                        onClick={handleClose}
-                        // className={...} // делает студент
-                    >
-                        Junior Plus
-                    </NavLink>
-                </nav>
-            </aside>
+                <aside className={sidebarClass}>
+                    <button className={s.close} onClick={handleClose}>
+                        <img
+                            src={closeIcon}
+                            alt="close sidebar"
+                            id={'hw5-menu-close'}
+                        />
+                    </button>
+
+                    <nav id={'hw5-menu'} className={s.nav}>
+                        <NavLink
+                            id={'hw5-pre-junior-link'}
+                            to={PATH.PRE_JUNIOR}
+                            onClick={handleClose}
+                            className={location.pathname === PATH.PRE_JUNIOR
+                                ? s.active
+                                : ''} // делает студент
+                        >
+                            Pre-junior
+                        </NavLink>
+                        <NavLink
+                            id={'hw5-junior-link'}
+                            to={PATH.JUNIOR}
+                            onClick={handleClose}
+                            className={location.pathname === PATH.JUNIOR
+                                ? s.active
+                                : ''} // делает студент
+                        >
+                            Junior
+                        </NavLink>
+                        <NavLink
+                            id={'hw5-junior-plus-link'}
+                            to={PATH.JUNIOR_PLUS}
+                            onClick={handleClose}
+                            className={location.pathname === PATH.JUNIOR_PLUS
+                                ? s.active
+                                : ''} // делает студент
+                        >
+                            Junior Plus
+                        </NavLink>
+                    </nav>
+                </aside>
+
         </>
     )
 }
