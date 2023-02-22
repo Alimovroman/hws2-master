cc0b030c5588410709fedb45b775255e6b8b7341
Roman <Alimov.ramon@gmail.com>
2023-02-22 19:41:01 +0300

    hw 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,33 @@
 import {UserType} from '../HW8'
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+type ActionType = ReturnType<typeof sortAC> | ReturnType<typeof checkAC>
+    // | { type: 'sort'; payload: 'up' | 'down' }
+    // | { type: 'check'; payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case "sort": { // by name
+            const result = action.payload === 'up'
+                ? [...state.sort((a,b) => a.name.localeCompare(b.name))]
+                : [...state.sort((a,b) =>  a.name.localeCompare(b.name)).reverse()]
+            return result// need to fix
         }
-        case 'check': {
+        case "check": {
 
-            return state // need to fix
+            return state.filter(u => u.age > action.payload).reverse() // need to fix
         }
         default:
             return state
     }
 }
+
+export const sortAC = (sortName: 'up' | 'down') => ({
+    type: 'sort',
+    payload: sortName
+} as const)
+
+export const checkAC = (age: number) => ({
+    type: 'check',
+    payload: age
+} as const)
